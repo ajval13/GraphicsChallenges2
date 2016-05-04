954de437036a5899a2e5f8394ee7ed2e278f85c8
alex.valdo <alex.valdo@MU-C1-07.mullen.pvt.k12.co.us>
2016-05-04 14:56:51 -0600

    up to challenge one

--- Graphics Challenges 2/src/CanvasB.java
+++ Graphics Challenges 2/src/CanvasB.java
@@ -6,9 +6,12 @@ import javax.swing.JFrame;
 public class CanvasB extends Canvas
 	{
 	private static final long	serialVersionUID	= 1L;
-
+		static int sizeHeight;
+		static String color;
+		static int sizeWidth;
 		public static void main(String[] args)
 			{
+			makeSquare();
 			CanvasB canvas = new CanvasB();
 		    JFrame frame = new JFrame();
 		    frame.setSize(1000, 1000);
@@ -17,10 +20,25 @@ public class CanvasB extends Canvas
 		    frame.setLocationRelativeTo(null); //Sets JPanel to center of screen 
 		    frame.setResizable(false);
 		    frame.setVisible(true);
+		    
 			}
+		
+		public static void makeSquare()
+		   {
+			Scanner userInput = new Scanner (System.in);
+			System.out.println("How tall you want the square. Must Be less than 1000.");
+			sizeHeight = userInput.nextInt();
+			Scanner userInput3 = new Scanner (System.in);
+			System.out.println("How wide you want the square. Must Be less than 1000.");
+			sizeWidth = userInput3.nextInt();
+			Scanner userInput2 = new Scanner (System.in);
+			System.out.println("Want it to be red or blue?");
+			color = userInput2.nextLine();
+		   }
 		public void paint(Graphics graphics)
-		{
-			
-		}
+		    {
+			graphics.setColor(Color.getColor(color));
+			graphics.fillRect(0, 0,sizeWidth,sizeHeight);
+		    }
 	}
 
